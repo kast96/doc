90da2d4c7f7e0529d568b56c075606b9325452d0
kast96 <kast96@yandex.ru>
2023-11-03 11:06:29 +0300

    upgrade docusarus to v3

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,8 +1,9 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const {themes} = require('prism-react-renderer');
+const lightTheme = themes.github;
+const darkTheme = themes.dracula;
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -123,9 +124,9 @@ const config = {
         copyright: `Copyright © ${new Date().getFullYear()} Экспансия, Разработан с помощью Docusaurus.`,
       },
       prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-        additionalLanguages: ['php', 'apacheconf'],
+        theme: lightTheme,
+        darkTheme: darkTheme,
+        additionalLanguages: ['bash', 'diff', 'json', 'php', 'apacheconf'],
       },
     }),
 };
